2a3ae453ab9632a49a3dd5f856faa239a7f92830
cubicArch <ysloaf@connect.ust.hk>
2025-04-23 02:27:28 +0800

    added lazygit support via snacks.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,6 +20,7 @@ return {
     statuscolumn = { enabled = true },
     words = { enabled = true },
     image = { enabled = false, doc = { enabled = true, inline = true }},
+    lazygit = {},
   },
   keys = {
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
@@ -43,6 +44,7 @@ return {
     { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
     { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
     { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    { "<leader>gg", function() Snacks.lazygit.open() end, desc = "Open Lazy[g]it" },
     -- Grep
     { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
     { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
