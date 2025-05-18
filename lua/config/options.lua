3019c4497eae8206c0ee9e388b07cf4e68576d16
cubicArch <ysloaf@connect.ust.hk>
2025-05-18 21:34:25 +0800

    fix: disable folding on startup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,9 +39,10 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 
-vim.opt.foldmethod = 'indent'
-
 vim.opt.conceallevel = 2
 vim.opt.showmode = false
+
+vim.opt.foldmethod = "indent"
+vim.cmd[[ set nofoldenable ]]
 
 -- vim: ts=2 sts=2 sw=2 et
