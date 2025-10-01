1a7f3aa3d13e828edeeeeb47870355739f1b39a9
cubicWindows <ysloaf@connect.ust.hk>
2025-10-01 15:19:51 +0800

    feat: added many statusline customization

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,15 +1,42 @@
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
+local function is_recording()
+    local reg = vim.fn.reg_recording()
+    if reg == "" then return "" end -- not recording
+    return "recording @" .. reg
+end
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         sections = {
-          lualine_a = {
-            {
-              'searchcount',
-              maxcount = 99999,
-              timeout = 500,
-            }
-          }
+            lualine_a = {
+                { 'mode' },
+            },
+            lualine_b = {
+                { is_recording },
+                { 'branch' },
+                { 'diff', source = diff_source }, 
+                { 'diagnostics' }
+            },
+            lualine_c = {
+              { 'filename' },
+              {
+                'searchcount',
+                maxcount = 99999,
+                timeout = 500,
+              }
+            },
         }
     }
 }
