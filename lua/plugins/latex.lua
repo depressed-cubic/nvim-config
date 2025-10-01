712d1e4d25944d24f35f9a6d7a639fb4d9aa6220
cubicWindows <ysloaf@connect.ust.hk>
2025-10-01 15:17:44 +0800

    feat: switch to using sioyek as pdf viewer for vimtex

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -4,8 +4,9 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_general_viewer = 'okular'
-    vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+    vim.g.vimtex_view_method = 'sioyek'
+    -- vim.g.vimtex_view_general_viewer = 'okular'
+    -- vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
     vim.g.vimtex_compiler_latexmk = {
          ['aux_dir'] = './aux',
          ['out_dir'] = '',
