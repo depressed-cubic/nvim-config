d3299f61416baab069550cc87e07667c2dbcf838
cubicArch <ysloaf@connect.ust.hk>
2025-10-07 20:34:30 +0800

    feat: trying haskell (need further fixes)

--- lua/plugins/haskell.lua
+++ lua/plugins/haskell.lua
@@ -1,5 +1,29 @@
 return {
-  'mrcjkb/haskell-tools.nvim',
-  version = '^6', -- Recommended
-  lazy = false, -- This plugin is already lazy
+    'mrcjkb/haskell-tools.nvim',
+    version = '^6', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function ()
+        vim.g.haskell_tools = {
+          ---@type haskell-tools.tools.Opts
+          tools = {
+            -- ...
+          },
+          ---@type haskell-tools.lsp.ClientOpts
+          ---You can also configure these via `:h vim.lsp.config`,
+          --- with the "haskell-tools" key.
+          hls = {
+            -- ---@param client number The LSP client ID.
+            -- ---@param bufnr number The buffer number
+            -- ---@param ht HaskellTools = require('haskell-tools')
+            -- on_attach = function(client, bufnr, ht)
+              -- Set keybindings, etc. here.
+            capabilities = require('blink.cmp').get_lsp_capabilities()
+            -- ...
+          },
+          ---@type haskell-tools.dap.Opts
+          dap = {
+            -- ...
+          },
+        }
+    end
 }
